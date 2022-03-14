af8ff9375f69b4d4ffa1074fc19bae16ef532108
jbfletch <amcdonald@confluent.io>
2022-03-14 11:12:48 -0400

    initial commit

---
+++ src/main/java/FunctionalProcessor.java
@@ -0,0 +1,9 @@
+import org.apache.kafka.streams.processor.api.ContextualProcessor;
+import org.apache.kafka.streams.processor.api.Record;
+
+public class FunctionalProcessor extends ContextualProcessor {
+    @Override
+    public void process(Record record) {
+
+    }
+}
